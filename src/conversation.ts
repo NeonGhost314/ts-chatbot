d0af305c142837142e6c7b7d5fa4052c9b04b292
a1hassane <144621932+NeonGhost314@users.noreply.github.com>
2025-12-04 22:26:57 -0500

    refactor: clean up imports and improve conversationOpen function structure

--- src/conversation.ts
+++ src/conversation.ts
@@ -1,8 +1,19 @@
-import { ClassifiedMessage ,Intent, Message , ConversationState} from "./types";
+import { ClassifiedMessage, Intent, Message, ConversationState } from "./types";
+import readline from "readline";
+import { createInterface } from "readline";
 
+export function getConversationState() {}
 
-
-export function getConversationState() {
-    
+//Learning about Readline from node.js https://nodejs.org/api/readline.html
+// will eventually be change for an user input in a frontend env
+function conversationOpen() {
+    while (true) {
+        const rl = readline.createInter({
+            input: process.stdin, // Readable stream to listen to
+            output: process.stdout, // Writable stream to write to
+            prompt: "> ", // Prompt to display (default: '> ')
+        });
+    }
 }
 
+console.log("ALLO");
